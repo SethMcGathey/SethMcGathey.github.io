51bb1c8956ffd88691aa7804860018d019a3a394
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-11-03 19:58:09 -0600

    setup basic layout for navigation and json

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+var jqxhr = $.getJSON( "js/blogEntries.json", function() {
+  console.log( "success" );
+})
+  .success(function() {
+    console.log( "second success" );
+  })
+  .error(function() {
+    console.log( "error" );
+  })
+  .complete(function() {
+    console.log( "complete" );
+  });
+ 
+// Perform other work here ...
+ 
+// Set another completion function for the request above
+jqxhr.complete(function() {
+  console.log( "second complete" );
+});
