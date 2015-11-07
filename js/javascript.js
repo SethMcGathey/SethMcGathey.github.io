01e3bc614b0e50c616978aa5fab82a865e3f2c89
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-11-07 16:42:55 -0600

    tried adding index to 1 in another variable

--- js/javascript.js
+++ js/javascript.js
@@ -6,10 +6,11 @@ $.getJSON( "js/blogEntries.json", function(data) {
   
   $.each(obj.entries, function(index, value)
   {
+    var num = index + 1;
     $(".blogPosts").append("<h5 class='centerText pagination'>" + value.date + "</h5>").hide();
     $(".blogPosts").append("<h5 class='centerText pagination'>" + value.title + "</h5>").hide();
     $(".blogPosts").append("<p class='centerText pagination'>" + value.text + "</p>").hide();
-    $(".pagination").append("<div class='paginationButtons'><a>" + index+1 + "</a></div>")
+    $(".pagination").append("<div class='paginationButtons'><a>" + index + "</a></div>");
     //$(".blogPosts").text(obj.entries[1].date + " " + obj.entries[1].title + " " + obj.entries[1].text);
   })
 })
