b34d026fecea0cb57eb61750ad80a10298d699ee
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-11-09 22:40:15 -0600

    changed the way I created the div

--- js/javascript.js
+++ js/javascript.js
@@ -11,9 +11,10 @@ $.getJSON( "js/blogEntries.json", function(data) {
   $.each(obj.entries, function(index, value)
   {
     numOfEntries++;
-    $(".blogPosts").append("<div class='profBoxes'><h5 class='centerText group" + numOfEntries + "'>" + value.date + "</h5>");//.hide();
-    $(".blogPosts").append("<h5 class='centerText group" + numOfEntries + "'>" + value.title + "</h5>");//.hide();
-    $(".blogPosts").append("<p class='centerText group" + numOfEntries + "'>" + value.text + "</p></div>");//.hide();
+    $(".blogPosts").append("<div class='profBoxes postContainer'></div>");
+    $(".postContainer").append("<h5 class='centerText group" + numOfEntries + "'>" + value.date + "</h5>");//.hide();
+    $(".postContainer").append("<h5 class='centerText group" + numOfEntries + "'>" + value.title + "</h5>");//.hide();
+    $(".postContainer").append("<p class='centerText group" + numOfEntries + "'>" + value.text + "</p>");//.hide();
     $(".group" + numOfEntries).hide();
     //$(".blogPosts").text(obj.entries[1].date + " " + obj.entries[1].title + " " + obj.entries[1].text);
   })
