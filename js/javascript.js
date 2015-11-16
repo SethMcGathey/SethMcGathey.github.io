854dbbaf5fc8454760f688fe37c7f6b359618a76
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-11-15 22:11:48 -0600

    removed it again

--- js/javascript.js
+++ js/javascript.js
@@ -83,7 +83,7 @@ function unhideGroupNext()
   current = number;
 }
 
-function searchFunction(word)
+/*function searchFunction(word)
 {
   $.each(obj.entries, function(index, value)
   {
@@ -99,7 +99,7 @@ function searchFunction(word)
     } 
     numOfEntries++;
   })
-}
+}*/
 
 
 
