ad21a70ec04084bf20dcc2a294f1ca6103656427
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-11-28 13:48:33 -0600

    moved the num incrementor before if statment

--- js/javascript.js
+++ js/javascript.js
@@ -167,9 +167,9 @@ function showPostsWhenWordFound(word)
     console.log(str.indexOf(word));
     
     console.log(str.indexOf("cat"));
+    num++;
     if(str.indexOf(word) != -1 || str1.indexOf(word) != -1 || str2.indexOf(word) != -1)
     {
-      num++;
       console.log("made it 3");
       $(".group" + num).show();
 
