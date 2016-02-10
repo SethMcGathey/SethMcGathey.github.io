b1b32b2c30e840265aa0074abe19056a627e1db5
unknown <sethm@mil-sethm-dt7.marketprobe.priv>
2016-02-10 16:22:27 -0600

    pushing all content

--- js/javascript.js
+++ js/javascript.js
@@ -175,6 +175,47 @@ $("#TeamProfile").click(function() {
 
 
 
+
+
+$( document ).ready( function() { 
+  $( "#wordpressHomePage" ).show(); 
+  $( "#wordpressContactPage" ).hide();
+  $( "#wordpressProductPage" ).hide();
+  $( "#wordpressTeamPage" ).hide();
+} );
+
+$('#wordpressHomeButton').click( function() { 
+  $( "#wordpressHomePage" ).show(); 
+  $( "#wordpressContactPage" ).hide();
+  $( "#wordpressProductPage" ).hide();
+  $( "#wordpressTeamPage" ).hide();
+} );
+
+$('#wordpressContactPageButton').click( function() { 
+  $( "#wordpressHomePage" ).hide(); 
+  $( "#wordpressContactPage" ).show();
+  $( "#wordpressProductPage" ).hide();
+  $( "#wordpressTeamPage" ).hide();
+} );
+
+$('#wordpressProductPageButton').click( function() { 
+  $( "#wordpressHomePage" ).hide(); 
+  $( "#wordpressContactPage" ).hide();
+  $( "#wordpressProductPage" ).show();
+  $( "#wordpressTeamPage" ).hide();
+} );
+
+$('#wordpressMeetTheTeamPageButton').click( function() { 
+  $( "#wordpressHomePage" ).hide(); 
+  $( "#wordpressContactPage" ).hide();
+  $( "#wordpressProductPage" ).hide();
+  $( "#wordpressTeamPage" ).show();
+} );
+
+
+
+
+
 /*function findString (str) {
  if (parseInt(navigator.appVersion)<4) return;
  var strFound;
